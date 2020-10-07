b0a990fc7b4503624bf8da4b1fe5a7beca07ac71
eagsuil <49492545+eagsuil@users.noreply.github.com>
2020-10-07 02:09:48 +0100

    Neu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import { NeuCard, NeuHeading, NeuProgressBar, NeuReaction,NeuInput,NeuButton } from 'neumorphic-ui';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App card">
+      <header>
+        <NeuHeading className="m-auto" text="Welcome to Neumorphic UI" fontSize="50px" fontWeight="900" />
       </header>
+      <div className="m-auto p-3">
+        <NeuCard width="350px" height="320px">
+          Neumorphic Card
+          <NeuReaction ><img src={logo} width="30px" height="30px" /></NeuReaction>
+        <div className="p-2">
+        <NeuProgressBar progress="80%" />
+      </div>
+      <NeuButton hovered={true} mouseOver={true} mouseOut={true} text="Neumorhpic Button" />
+        </NeuCard>
+      </div>
+      
     </div>
   );
 }
