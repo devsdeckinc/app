249ae79abd37ee89546d74e58ad0a8af6ff46684
eagsuil <49492545+eagsuil@users.noreply.github.com>
2020-10-07 01:42:58 +0100

    Updates

---
+++ precache-manifest.f3645c5f6d985acec41987f84a4d3afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d392c28e734bbd35d962c254567bce",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "e5a07a01595a77a3ffb4",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/app/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a07a01595a77a3ffb4",
+    "url": "/app/static/js/main.646b9656.chunk.js"
+  },
+  {
+    "revision": "307a9b8e3d72fb5305a6",
+    "url": "/app/static/js/runtime-main.079770c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
